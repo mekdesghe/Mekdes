1200ce1c708da69d0bcf72f36fa7a4a137531fdc
Sri <sri@rts-macbook-air.home>
2015-03-30 23:23:48 -0400

    added a mekdes class

---
+++ ccmek.h
@@ -0,0 +1,13 @@
+//
+//  ccmek.h
+//  Mekdes
+//
+//  Created by Sri on 3/30/15.
+//  Copyright (c) 2015 Mekdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ccmek : UIView
+
+@end
---
+++ ccmek.m
@@ -0,0 +1,21 @@
+//
+//  ccmek.m
+//  Mekdes
+//
+//  Created by Sri on 3/30/15.
+//  Copyright (c) 2015 Mekdi. All rights reserved.
+//
+
+#import "ccmek.h"
+
+@implementation ccmek
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
