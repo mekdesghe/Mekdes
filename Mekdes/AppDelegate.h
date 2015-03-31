9f112a92fde211309af5ea174be193c77ea6472d
Sri <sri@rts-macbook-air.home>
2015-03-30 23:07:00 -0400

    Initial Commit

---
+++ Mekdes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mekdes
+//
+//  Created by Sri on 3/30/15.
+//  Copyright (c) 2015 Mekdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mekdes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mekdes
+//
+//  Created by Sri on 3/30/15.
+//  Copyright (c) 2015 Mekdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
